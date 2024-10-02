dea28ce3e50e15a98a755e300502810890bacf42
inma <inmarodriguez@ub.edu>
2024-10-02 08:34:51 +0200

    una classe i un enunciat que faltaven

---
+++ Sequencials-Alternatives/src/UltimDigit.java
@@ -0,0 +1,24 @@
+/*
+ * Autor:  Nom Cognom1 Cognom2
+ * Data:   dd/mm/aa
+ * Versió: 1.0
+ */
+
+/*
+ * P1 - Prog 24-25
+ * Exercici 1.	Feu un programa que donat un nombre enter introduït
+ * per teclat per l'usuari, mostra per pantalla la seva última xifra.
+ * Per exemple, si l’usuari introdueix 143, es mostra per pantalla el 3.
+ */
+
+
+/* Taula de tests
+  Entrada            | Sortida esperada
+  -------------------------------------
+  123                |   3
+  4                  |   4
+  78956              |   6
+  2147483648		     |   error d'execucio: java mostra una excepció per enter fora de rang
+*/
+public class UltimDigit {
+}
