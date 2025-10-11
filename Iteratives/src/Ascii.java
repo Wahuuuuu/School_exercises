fdf9bac2544c62c73c140d45668fec7dbb05383d
Yanzhe Chen <ychenche1179@alumnes.ub.edu>
2025-10-11 23:58:12 +0200

    11/10/2025

--- Iteratives/src/Ascii.java
+++ Iteratives/src/Ascii.java
@@ -22,7 +22,16 @@
                      |
   		               |
 */
+
+import java.util.Scanner
 public class Ascii {
     public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	String word;
+
+	word = sc.next();
+	for (int i = 0; i < word.length(); ++i){
+		System.out.println(word[i] + int(word[i]));
+	}
     }
 }
