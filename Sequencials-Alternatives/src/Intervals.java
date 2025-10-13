3954897a1d90b615750df1a796f69386f889dd17
Yanzhe Chen <ychenche1179@alumnes.ub.edu>
2025-10-13 23:40:31 +0200

    13/10/2025

--- Sequencials-Alternatives/src/Intervals.java
+++ Sequencials-Alternatives/src/Intervals.java
@@ -16,10 +16,32 @@
 /* Taula de tests
    Entrada            | Sortida esperada
   -------------------------------------
-                     |
-                     |
-                     |
-  		               |
+   2.0 4.4 5.0 6.3    | Els intervals no intersequen
+   2.0 4.4 3.0 6.3    | Els intervals intersequen
+   5.0 6.3 2.0 4.4    | Els intervals no intersequen
+   3.0 6.3 2.0 4.4    | Els intervals intersequen
 */
+
+
+import java.util.Scanner;
 public class Intervals {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float x1, x2, y1, y2;
+
+		System.out.println("Indroduerix dos intervals descrits pels quatre nombres reals: ");
+		x1 = sc.nextFloat();
+	        x2 = sc.nextFloat();
+		y1 = sc.nextFloat();
+		y2 = sc.nextFloat();
+
+		// Els intervals intersequen quan x2 >= y1 o y2 >= x1
+
+		if ((x1 <= y1 && x2 >= y1) || (x1 >= y1 && x1 <= y2)){
+			System.out.println("Els intervals intersequen");
+		}
+		else {
+			System.out.println("Els intervals no intersequen");
+		}
+	}
 }
