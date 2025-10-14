5f37d0786c064d4315acf9bbb3047fb1111b60ff
Yanzhe Chen <ychenche1179@alumnes.ub.edu>
2025-10-14 13:22:52 +0200

    14/10/2025

--- Sequencials-Alternatives/src/Concat.java
+++ Sequencials-Alternatives/src/Concat.java
@@ -1,6 +1,6 @@
 /*
- * Autor:  Nom Cognom1 Cognom2
- * Data:   dd/mm/aa
+ * Autor:  Yanzhe Chen
+ * Data:   14/10/2025
  * Versió: 1.0
  */
 
@@ -10,18 +10,25 @@
  * S’ha de fer ús del mètode append() de la classe StringBuffer.
  * Perquè creus que no utilitzem la classe String? (Concat.java)
  */
-
+// Perquè String es una classe inmutable
 
 /* Taula de tests
   Entrada            | Sortida esperada
   -------------------------------------
-                     |
-                     |
-                     |
-  		               |
+  in mutable         | inmutable
+  12 3456            | 123456
+  $% &/()            | $%&/()
 */
+import java.util.Scanner;
 public class Concat {
     public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+    	StringBuffer sb = new StringBuffer();
+
         System.out.println("Prova des de Concat");
+	sb.append(sc.next());
+	sb.append(sc.next());
+
+	System.out.println(sb);
     }
 }
