c2b4d81b48759bdf9852172fb7e1703aafcdd8ed
Yanzhe Chen <ychenche1179@alumnes.ub.edu>
2025-10-15 23:15:42 +0200

    15/10/2025

--- Sequencials-Alternatives/src/Maxof3Amb2if.java
+++ Sequencials-Alternatives/src/Maxof3Amb2if.java
@@ -1,6 +1,6 @@
 /*
- * Autor:  Nom Cognom1 Cognom2
- * Data:   dd/mm/aa
+ * Autor:  Yanzhe Chen
+ * Data:   15/10/2025
  * Versió: 1.0
  */
 
@@ -12,10 +12,30 @@
 /* Taula de tests
    Entrada            | Sortida esperada
   -------------------------------------
-                     |
-                     |
-                     |
-  		               |
+   1 2 3              | 3
+   -10086 -3 0        | 0
+   0 0 0              | 0
 */
+
+import java.util.Scanner;
 public class Maxof3Amb2if {
+	public static void main(String[] arg){
+		Scanner sc = new Scanner(System.in);
+		int n1, n2, n3, max;
+
+		System.out.println("Introdueix 3 nombres enters: ");
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		n3 = sc.nextInt();
+
+		max = n1;
+		if ((n2 > n1) && (n2 > n3)) {
+			max = n2;
+		}
+		else if (n3 > n1) {
+			max = n3;
+		}
+
+		System.out.println(max);
+	}
 }
