87c8f87eaac51aa671287dd70c57f318df664c3e
Yanzhe Chen <ychenche1179@alumnes.ub.edu>
2025-10-20 17:15:13 +0200

    20/10/2025

--- Iteratives/src/Contentes.java
+++ Iteratives/src/Contentes.java
@@ -1,6 +1,6 @@
 /*
- * Autor:  Nom Cognom1 Cognom2
- * Data:   dd/mm/aa
+ * Autor:  Yanzhe Chen
+ * Data:   19/10/2025
  * Versió: 1.0
  */
 
@@ -18,10 +18,48 @@
 /* Taula de tests
   Entrada            | Sortida esperada
   -------------------------------------
-                     |
-                     |
-                     |
-  		               |
+  zdaaaoa            | No totes les a estan contentes
+  zdaa               | Totes les a estan contentes
+  AaaAaaaa           | Totes les a estan contentes
+  bbbb               | Totes les a estan contentes
 */
+
+import java.util.Scanner;
 public class Contentes {
+	public static void main(String[] arg){
+		Scanner sc = new Scanner(System.in);
+		String  p;
+
+		System.out.println("Indica una palaura: ");
+		p = sc.next();
+		int length = p.length();
+
+		boolean contenta = true;
+		/*
+		 * No contenta if:
+		 * - palabra = 'a', OR
+		 * - first char is 'a', and second char is not 'a' OR
+		 * - last char is 'a', and second last char is not 'a'
+		 */
+		if ( (length < 2 && (p.charAt(0) == 'a'))
+		  || (p.charAt(0) == 'a' && (p.charAt(1) != 'a'))
+		  || (p.charAt(length - 1) == 'a' && (p.charAt(length - 2) != 'a')) ){
+			contenta = false;
+		}
+
+		int i = 1;
+		while (i < length - 1 && contenta){
+			if (p.charAt(i) == 'a'){
+				if (p.charAt(i - 1) != 'a' && (p.charAt(i + 1) != 'a')){
+					contenta = false;
+				}
+			}
+			i++;
+		}
+
+		if (contenta) System.out.println("Totes les a estan contentes");
+		else System.out.println("No totes les a estan contentes");
+
+		
+	}
 }
